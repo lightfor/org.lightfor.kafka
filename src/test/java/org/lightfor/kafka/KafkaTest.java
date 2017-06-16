80ccc8bc6c323d90c9d7a52db27623324f410e36
lightfor <death_light@foxmail.com>
2017-06-16 09:49:21 +0800

    pom, log4j config, kafka producer and consumer

---
+++ src/test/java/org/lightfor/kafka/KafkaTest.java
@@ -0,0 +1,57 @@
+package org.lightfor.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Kafka Test
+ * Created by Light on 2017/6/15.
+ */
+public class KafkaTest {
+    @Test
+    public void consumer(){
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "223.203.208.99:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("log-console"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+        }
+
+    }
+
+    @Test
+    public void producer(){
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.1.59:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for(int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<>("my-topic", Integer.toString(i), Integer.toString(i)));
+
+        producer.close();
+
+    }
+}
